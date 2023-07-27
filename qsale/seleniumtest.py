9358555e158fa41c7ec3d122a8850bd0ef2930ac
Rahib k <rahiofcl@gmail.com>
2023-07-27 10:06:56 +0000

    updated qsale

--- qsale/seleniumtest.py
+++ qsale/seleniumtest.py
@@ -1,6 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-driver = webdriver.Chrome()
+service = Service('/workspace/Gitpod-Projects/qsale/chromedriver_win32/chromedriver.exe')
+
+service.start()
+
+driver = webdriver.Remote(service.service_url)
+
+driver.get('http://www.google.com/');
 
 input("Press Enter key to quit")
 
